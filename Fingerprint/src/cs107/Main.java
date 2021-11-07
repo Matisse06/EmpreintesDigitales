5b129e50765161cd24a737bb0dcd6f86633b6ef4
Matisse Van Schalkwijk <matisse.vanschalkwijk@epfl.ch>
2021-11-07 21:37:12 +0100

    derniere version

--- Fingerprint/src/cs107/Main.java
+++ Fingerprint/src/cs107/Main.java
@@ -27,15 +27,15 @@ public class Main {
     //testApplyRotation();
     //testApplyTranslation();
     //testThin();
-    //testWithSkeleton();
+    testWithSkeleton();
     
     //testDrawSkeleton("1_1"); //draw skeleton of fingerprint 1_1.png
     //testDrawSkeleton("1_2"); //draw skeleton of fingerprint 1_2.png
     //testDrawSkeleton("2_1"); //draw skeleton of fingerprint 2_1.png
 
     //testDrawMinutiae("1_1"); //draw minutiae of fingerprint 1_1.png
-    testDrawMinutiae("1_2"); //draw minutiae of fingerprint 1_2.png
-    testDrawMinutiae("2_1"); //draw minutiae of fingerprint 2_1.png
+    //testDrawMinutiae("1_2"); //draw minutiae of fingerprint 1_2.png
+    //testDrawMinutiae("2_1"); //draw minutiae of fingerprint 2_1.png
 	
     //---------------------------
     // Test overall functionality
